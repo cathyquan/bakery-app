92427e6326c8e9e7f3ca7424d1ebf20b581bfdce
Cathy Quan <cathy.t.quan@gmail.com>
2023-01-17 18:09:29 -0500

    small change

--- bakery_app/bakery_app/ContentView.swift
+++ bakery_app/bakery_app/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack { // change
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
