b83a2ab482adcd7acc5cd9fa3d9eab65de60cf75
Cathy Quan <cathy.t.quan@gmail.com>
2023-01-17 17:59:44 -0500

    created new project

---
+++ bakery_app/bakery_app/bakery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bakery_appApp.swift
+//  bakery_app
+//
+//  Created by Cathy Quan on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct bakery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
